0a927db8b06fd6abc68b135249d600543d3f60ac
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-06-17 06:19:54 +0530

    Salt and utils, no recursive delete

--- src/main/java/io/github/drmanan/whisper/util/Salt.java
+++ src/main/java/io/github/drmanan/whisper/util/Salt.java
@@ -1,4 +1,16 @@
 package io.github.drmanan.whisper.util;
 
 public class Salt {
+    private byte[] salt;
+
+    public Salt() {
+    }
+
+    public Salt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
 }
--- src/main/java/io/github/drmanan/whisper/util/Utils.java
+++ src/main/java/io/github/drmanan/whisper/util/Utils.java
@@ -2,17 +2,29 @@ package io.github.drmanan.whisper.util;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class Utils {
 
+    public static boolean isEmpty(String s) {
+        return s == null || s.trim().equals("");
+    }
+
+    public static Salt generateSalt() {
+        SecureRandom sr = new SecureRandom();
+        byte[] output = new byte[256];
+        sr.nextBytes(output);
+        return new Salt(output);
+    }
+
     public static String toHexString(byte[] bytes) {
-        char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         char[] hexChars = new char[bytes.length * 2];
         int v;
-        for ( int j = 0; j < bytes.length; j++ ) {
+        for (int j = 0; j < bytes.length; j++) {
             v = bytes[j] & 0xFF;
-            hexChars[j*2] = hexArray[v/16];
-            hexChars[j*2 + 1] = hexArray[v%16];
+            hexChars[j * 2] = hexArray[v / 16];
+            hexChars[j * 2 + 1] = hexArray[v % 16];
         }
         return new String(hexChars);
     }
@@ -25,9 +37,23 @@ public class Utils {
 
         byte[] result = digest.digest();
 
-        return  toHexString(result);
+        return toHexString(result);
     }
 
+   /*
+   // TODO
+   >> Commons-io or jdk.jpackage.internal.IOUtils or my own, to reduce the size and remove commons-io\
+   >> jdk.jpackage.internal.IOUtils.deleteRecursive returns void, may need acknowledgement of deletion
 
-
+   public static boolean recursiveDelete(File path) throws FileNotFoundException {
+        if (!path.exists()) throw new FileNotFoundException(path.getAbsolutePath());
+        boolean ret = true;
+        if (path.isDirectory()){
+            for (File f : path.listFiles()){
+                ret = ret && recursiveDelete(f);
+            }
+        }
+        return ret && path.delete();
+    }
+    */
 }
